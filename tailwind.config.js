58e37277fd5a191505fd2d1097a1928160f2b245
ismailarilik <arilik.ismail@gmail.com>
2022-09-21 09:29:25 +0300

    Watch index.js file for Tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/index.html'],
+  content: [
+    './public/index.html',
+    './public/javascripts/index.js'
+  ],
   theme: {
     extend: {}
   },
