dd0d2b9ffaa2f85587cb060018bbf019c6640f4b
ismailarilik <arilik.ismail@gmail.com>
2022-10-05 16:39:32 +0300

    Handle errors on create

--- routes/comments.js
+++ routes/comments.js
@@ -22,24 +22,30 @@ router.get('/', async (req, res, next) => {
 /* Create a comment */
 router.post('/', async (req, res, next) => {
   const commentPayload = req.body.comment
-  const comment = await Comment.create({
-    username: commentPayload.username,
-    avatar: commentPayload.avatar,
-    comment_date: commentPayload.commentDate,
-    comment_text: commentPayload.commentText,
-    upvotes: commentPayload.upvotes,
-    parent_comment_id: commentPayload.parentCommentId
-  })
-  res.send({
-    comment: {
-      username: comment.username,
-      avatar: comment.avatar,
-      commentDate: comment.comment_date,
-      commentText: comment.comment_text,
-      upvotes: comment.upvotes,
-      parentCommentId: comment.parent_comment_id
-    }
-  })
+  try {
+    const comment = await Comment.create({
+      username: commentPayload.username,
+      avatar: commentPayload.avatar,
+      comment_date: commentPayload.commentDate,
+      comment_text: commentPayload.commentText,
+      upvotes: commentPayload.upvotes,
+      parent_comment_id: commentPayload.parentCommentId
+    })
+    res.send({
+      comment: {
+        username: comment.username,
+        avatar: comment.avatar,
+        commentDate: comment.comment_date,
+        commentText: comment.comment_text,
+        upvotes: comment.upvotes,
+        parentCommentId: comment.parent_comment_id
+      }
+    })
+  } catch (e) {
+    console.log(e)
+    // NOTE: The status code 500 is intentional; it is thought as a general placeholder indicates that an error exists
+    res.status(500).send(e.message)
+  }
 })
 
 /* Update a comment */
@@ -58,10 +64,20 @@ const updateComment = async (req, res, next) => {
     res.sendStatus(200)
   } catch (e) {
     console.log(e)
+    // NOTE: The status code 500 is intentional; it is thought as a general placeholder indicates that an error exists
     res.status(500).send(e.message)
   }
 }
 router.patch('/:id', updateComment)
 router.put('/:id', updateComment)
+
+
+
+router.get('/destroy', async (req, res, next) => {
+  await Comment.destroy({ truncate: true })
+  res.send('Oldu')
+})
+
+
 
 module.exports = router
