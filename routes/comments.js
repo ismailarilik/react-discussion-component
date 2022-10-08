2fc4abcf36bf4af76078c949de8b98b0d9d59adc
ismailarilik <arilik.ismail@gmail.com>
2022-10-08 11:10:47 +0300

    Allow one level of comment nesting

--- routes/comments.js
+++ routes/comments.js
@@ -29,24 +29,43 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   const commentPayload = req.body.comment
   try {
-    const comment = await Comment.create({
-      username: commentPayload.username,
-      avatar: commentPayload.avatar,
-      comment_date: commentPayload.commentDate,
-      comment_text: commentPayload.commentText,
-      upvotes: commentPayload.upvotes,
-      parent_comment_id: commentPayload.parentCommentId
-    })
-    res.send({
-      comment: {
-        username: comment.username,
-        avatar: comment.avatar,
-        commentDate: comment.comment_date,
-        commentText: comment.comment_text,
-        upvotes: comment.upvotes,
-        parentCommentId: comment.parent_comment_id
+    const createComment = async () => {
+      const comment = await Comment.create({
+        username: commentPayload.username,
+        avatar: commentPayload.avatar,
+        comment_date: commentPayload.commentDate,
+        comment_text: commentPayload.commentText,
+        upvotes: commentPayload.upvotes,
+        parent_comment_id: commentPayload.parentCommentId
+      })
+      res.send({
+        comment: {
+          username: comment.username,
+          avatar: comment.avatar,
+          commentDate: comment.comment_date,
+          commentText: comment.comment_text,
+          upvotes: comment.upvotes,
+          parentCommentId: comment.parent_comment_id
+        }
+      })
+    }
+
+    // For v2, one level of nesting is allowed
+    if (commentPayload.parentCommentId) {
+      const parentComment = await Comment.findOne({
+        where: {
+          id: commentPayload.parentCommentId
+        }
+      })
+      if (parentComment.parentCommentId !== undefined && parentComment.parentCommentId !== null) {
+        // So it means there is a hack to nest a comment more than one level; send necessary answer back
+        res.status(422).send('Nesting comments more than one level is not allowed in v2.')
+      } else {
+        createComment()
       }
-    })
+    } else {
+      createComment()
+    }
   } catch (e) {
     console.log(e)
     // NOTE: The status code 500 is intentional; it is thought as a general placeholder indicates that an error exists
